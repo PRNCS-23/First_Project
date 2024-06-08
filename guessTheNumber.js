34d8fa574a008f93f95b0e460bf67dd6abbdde1c
PRNCS-23 <prncs23042004@gmail.com>
2024-06-08 23:27:07 +0000

    half Game is done

---
+++ guessTheNumber.js
@@ -0,0 +1,32 @@
+const randomNumber = parseInt((Math.random() * 100 + 1))
+
+const userInput = document.querySelector('#guessField');
+const Submit = document.querySelector('#subt');
+const resultDisplay = document.querySelector('resultParas');
+const previousGuess = document.querySelector('.guesses');
+const guessRemaining = document.querySelector(".lastResult");
+
+// TO CHECK WHETER THE USER INPUT IS VALID OR NOT
+function validGuest(guess){
+    //
+}
+
+// TO CHECK WHETER THE USER GUESS IS RIGHT OR NOT
+function checkGuess(guess){
+    //
+} 
+
+// TO DISPLAY THE GUESS WHETER THE INPUT NUMBER IS RIGHT OR NOT 
+function displayGuess(guess){
+    //
+}
+
+// TO DISPLAY MESSAGE TO USER 
+function displayMessage(Message){
+    //
+}
+
+// END GAME
+function endGame(){
+    //
+}
